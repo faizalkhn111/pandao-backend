185f95839f6e940e0c6e452495518da14dd65a68
Your Name <faizal.khan@apnttech.com>
2024-10-14 23:20:15 +0530

    add-proposal-comment-as-activty

--- app/api/manifest_routes.py
+++ app/api/manifest_routes.py
@@ -216,7 +216,11 @@ def transaction_manifest_routes(app):
                                     )
                                     Enum<1u8>(
                                     Decimal("40")
+                                    )Enum<1u8>(
+                                    Address("account_tdx_2_128e6fmjkhjqx0n8h9562rrvstl883wq22pzea4ucnnx0762ptlch4s"
                                     )
+
+                                    
 
                                     ;
             """
